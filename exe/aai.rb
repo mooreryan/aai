57d5e592b4b70d8aaddc9b775b32fa2ec78114d5
Ryan Moore <moorer@udel.edu>
2017-06-20 01:09:26 -0400

    Clean up

--- exe/aai.rb
+++ exe/aai.rb
@@ -16,6 +16,3 @@ opts = Trollop.options do
   # opt(:infile, "Input file", type: :string)
   # opt(:outdir, "Output directory", type: :string, default: ".")
 end
-
-Aai.check_files ARGV
-Aai.process_input_seqs! ARGV
